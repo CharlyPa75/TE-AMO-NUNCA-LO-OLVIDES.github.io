00ef8ff430f4a2eac5243a0bb4114afc8ff0a58c
CharlyPa75 <118085459+CharlyPa75@users.noreply.github.com>
2023-03-09 18:57:32 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞 Now zomos Novios 💞');
+    alert('😱Nuestra VoDa Es MañaNa😱');
+    location.href = 'https://www.youtube.com/watch?v=TVY4fQEHmLk&ab_channel=songsSub';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('🥰 SOLO DIME QUE ME AMAS 🥰');
+  heroTitle.innerHTML += partner + ' ❤';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
